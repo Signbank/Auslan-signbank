e53c36272dd72ede77e13f6919eb3b6b8fec53b3
steve <steve@localhost>
2009-01-04 13:44:03 +0000

    Page menu now includes sub-pages of toplevel entries. Reorganised overall menu structure.

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -2,7 +2,13 @@ from auslan.pages.models import Page
 
 
 def menu(request):
-    """Generate a menu hierarchy from the current set of pages"""
+    """Generate a menu hierarchy from the current set of pages
+    
+    Returns a dictionary with keys 'home' and 'toplevel', the
+    entries of these are lists of dictionaries each with
+    keys 'url', 'title' and 'children', the value of 'children'
+    is a similar list of dictionaries.
+    """
     
     menu = {'home': [], 'toplevel': []}
 
@@ -10,17 +16,26 @@ def menu(request):
     homes = Page.objects.filter(title="Home")
     if len(homes) >= 1:
         home = homes[0]
-        for page in Page.objects.filter(parent=home, publish=True):
-            menu['home'].append({'url': page.url, 'title': page.title})
+        menu['home'] = find_children(home)
     else:
         home = None
     # find toplevel pages, with no parent
-    toplevel = Page.objects.filter(parent=None, publish=True)
-    for page in toplevel:
-        if page != home:
-            menu['toplevel'].append({'url': page.url, 'title': page.title})
+    menu['toplevel'] = find_children(None)
     
     # return a dictionary to be merged with the request context
     return {'menu': menu}
+    
+
+def find_children(page):
+    """Find the child pages of a given page,
+    return a list of dictionaries suitable for insertion
+    into the menu structure described in menu()"""
+    
+    result = []
+    for page in Page.objects.filter(parent=page, publish=True):
+        if not page.title == "Home":
+            result.append({'url': page.url, 'title': page.title, 'children': find_children(page)})
+            
+    return result
     
     
