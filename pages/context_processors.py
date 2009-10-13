8381874d4f11a9bd82db8353024599a58bc4a119
steve <steve@localhost>
2009-10-13 21:17:34 +0000

    change menu style to an always visible submenu, still needs work

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -11,21 +11,29 @@ def menu(request):
     """
     
     # find toplevel pages, with no parent and thier children
-    menu = find_children(None)
-    
+    (menu, ignore) = find_children(None, request.META['PATH_INFO']) 
     # return a dictionary to be merged with the request context
     return {'menu': menu}
     
 
-def find_children(page):
+def find_children(page, currentURL):
     """Find the child pages of a given page,
     return a list of dictionaries suitable for insertion
     into the menu structure described in menu()"""
-    
+
+    isCurrent = False
+    anyCurrent = False
     result = []
     for page in Page.objects.filter(parent=page, publish=True).order_by('index'):
-        result.append({'url': page.url, 'title': page.title, 'children': find_children(page)})
-            
-    return result
+        (children, childCurrent) = find_children(page, currentURL)
+        # we're the current page if any of our children are the current page
+        # or if we're the current page        
+        isCurrent = ((page.url==currentURL) or childCurrent)
+        
+        # remember if any of the children are the current page
+        anyCurrent = (isCurrent or anyCurrent)
+        result.append({'url': page.url, 'title': page.title, 'children': children, 'current': isCurrent})
+
+    return (result, anyCurrent)
     
     
