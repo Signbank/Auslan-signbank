2c4fd0a858e43be59caf28f54c51b8a4d8eaea45
stevecassidy <stevecassidy@localhost>
2011-06-10 10:34:40 +1000

    Add dominant hand shape filter

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -64,7 +64,7 @@ class GlossAdmin(admin.ModelAdmin):
     save_on_top = True
     list_display = ['idgloss', 'annotation_idgloss', 'morph', 'sense', 'sn']
     search_fields = ['^idgloss', '=sn']
-    list_filter = ['InMedLex', 'healthtf', 'inWeb', 'bsltf', 'sense', 'auslextf']
+    list_filter = ['InMedLex', 'healthtf', 'inWeb', 'bsltf', 'sense', 'auslextf', 'domhndsh']
     inlines = [ RelationInline, DefinitionInline, TranslationInline ]
 
 
