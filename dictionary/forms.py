a40d7d228cf4931f3d766989b4d567504e82adb3
stevecassidy <stevecassidy@localhost>
2011-07-04 20:17:33 +1000

    Move the list of fields in the quick updates form into settings.py, change the set to just language and dialect for BSL.

--- dictionary/forms.py
+++ dictionary/forms.py
@@ -2,23 +2,13 @@ from django import forms
 from django.contrib.formtools.preview import FormPreview 
 from signbank.video.fields import VideoUploadToFLVField
 from signbank.dictionary.models import Dialect, Gloss
-
-
-class GlossUpdateForm(forms.Form):
-    """Form for updating selective elements of a gloss"""
-     
-    # fields used from the main update page
-    inWeb = forms.BooleanField(label="Include in the web dictionary?", required=False)
-    inMedLex = forms.BooleanField(label="Problematic Medical Sign?", required=False)
-    healthtf = forms.BooleanField(label="Medical Sign?", required=False)
-    keyword = forms.CharField(max_length=50, widget=forms.HiddenInput, required=False)
-    n = forms.IntegerField(widget=forms.HiddenInput, required=False)
-    dialect = forms.ModelMultipleChoiceField(label="Dialect", queryset=Dialect.objects.all(), required=False)#, widget=forms.CheckboxSelectMultiple)
+from django.conf import settings
 
 class GlossModelForm(forms.ModelForm):
     class Meta:
         model = Gloss
-        fields = ['inWeb', 'InMedLex', 'healthtf', 'language', 'dialect']
+        # fields are defined in settings.py
+        fields = settings.QUICK_UPDATE_GLOSS_FIELDS
   
     
 class VideoUpdateForm(forms.Form):
