f432c11861bc700c00a5bbd3d513884c77b25cc5
stevecassidy <stevecassidy@localhost>
2012-10-19 11:47:43 +1100

    Add command to pre-generate all jpeg images

---
+++ signbank/video/management/commands/makejpeg.py
@@ -0,0 +1,25 @@
+"""Convert a video file to flv"""
+
+from django.core.exceptions import ImproperlyConfigured
+from django.core.management.base import BaseCommand, CommandError  
+from django.conf import settings
+from signbank.video.models import GlossVideo
+import os
+
+class Command(BaseCommand):
+     
+    help = 'Create JPEG images for all videos'
+    args = ''
+
+    def handle(self, *args, **options):
+        
+        # just access the poster path for each video
+        for vid in GlossVideo.objects.all():
+            p = vid.poster_path()
+            print p
+     
+        else:
+            print "Usage makejpeg"
+
+
+    
