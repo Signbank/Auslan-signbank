14b70722bb19088333db4465e358902b8d8ae779
Steve Cassidy <Steve.Cassidy@mq.edu.au>
2012-11-18 16:14:16 +1100

    pass force arg to convert video, only make directories if they don't exist

--- signbank/video/management/commands/convertvideo.py
+++ signbank/video/management/commands/convertvideo.py
@@ -30,14 +30,16 @@ def convert_video_collection(sourcedir, destdir):
     
     for dir in os.listdir(sourcedir):
         if os.path.isdir(os.path.join(sourcedir, dir)):
-            os.makedirs(os.path.join(destdir, dir))
+            if not os.path.exists(os.path.join(destdir, dir)):
+                os.makedirs(os.path.join(destdir, dir))
+
             for f in os.listdir(os.path.join(sourcedir, dir)):
                 (name, ext) = os.path.splitext(f)
                 if ext in ['.mp4', '.flv']:
                     sourcefile = os.path.join(sourcedir, dir, f)
                     destfile = os.path.join(destdir, dir, name+".mp4")
                     print sourcefile, destfile
-                    convert_video(sourcefile, destfile)
+                    convert_video(sourcefile, destfile, force=True)
                     
 
 
