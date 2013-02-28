f6ff3c8523e2c9c4ec9804f88e84ab719acacce6
Steve Cassidy <Steve.Cassidy@mq.edu.au>
2013-02-28 13:37:00 +1100

    Use an alternate URL to deliver video to mobile devices.

--- signbank/video/views.py
+++ signbank/video/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import Context, RequestContext
-from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.decorators import login_required
 from models import Video, GlossVideo
 from forms import VideoUploadForm, VideoUploadForGlossForm
 from convertvideo import extract_frame
+import django_mobile
 
-def addvideo(request): 
+def addvideo(request):
     """View to present a video upload form and process
     the upload"""
-       
+
     if request.method == 'POST':
-        
+
         form = VideoUploadForGlossForm(request.POST, request.FILES)
         if form.is_valid():
             sn = form.cleaned_data['gloss_sn']
             vfile = form.cleaned_data['videofile']
             vfile.name = sn+".mp4"
             redirect_url = form.cleaned_data['redirect']
-                        
+
             # deal with any existing video for this sign
             oldvids = GlossVideo.objects.filter(gloss_sn=sn)
             for v in oldvids:
@@ -25,15 +26,15 @@ def addvideo(request):
 
             video = GlossVideo(videofile=vfile, gloss_sn=sn)
             video.save()
-            
-            
+
+
             # TODO: provide some feedback that it worked (if
             # immediate display of video isn't working)
             return redirect(redirect_url)
-    
-    # if we can't process the form, just redirect back to the 
+
+    # if we can't process the form, just redirect back to the
     # referring page, should just be the case of hitting
-    # Upload without choosing a file but could be 
+    # Upload without choosing a file but could be
     # a malicious request, if no referrer, go back to root
     if request.META.has_key('HTTP_REFERER'):
         url = request.META['HTTP_REFERER']
@@ -45,7 +46,7 @@ def addvideo(request):
 def deletevideo(request, videoid):
     """Remove the video for this gloss, if there is an older version
     then reinstate that as the current video (act like undo)"""
-    
+
     if request.method == "POST":
         # deal with any existing video for this sign
         vids = GlossVideo.objects.filter(gloss_sn=videoid).order_by('version')
@@ -53,10 +54,10 @@ def deletevideo(request, videoid):
             # this will remove the most recent video, ie it's equivalent
             # to delete if version=0
             v.reversion(revert=True)
-            
+
     # TODO: provide some feedback that it worked (if
     # immediate non-display of video isn't working)
-    
+
     # return to referer
     if request.META.has_key('HTTP_REFERER'):
         url = request.META['HTTP_REFERER']
@@ -64,32 +65,37 @@ def deletevideo(request, videoid):
         url = '/'
     return redirect(url)
 
-    
+
 def poster(request, videoid):
-    """Generate a still frame for a video (if needed) and 
+    """Generate a still frame for a video (if needed) and
     generate a redirect to the static server for this frame"""
-    
+
     video = get_object_or_404(GlossVideo, gloss_sn=videoid)
-    
+
     return redirect(video.poster_url())
 
 
 def video(request, videoid):
     """Redirect to the video url for this videoid"""
-    
+
     video = get_object_or_404(GlossVideo, gloss_sn=videoid)
-    
+
     return redirect(video)
-    
+
 def iframe(request, videoid):
     """Generate an iframe with a player for this video"""
-    
+
     video = get_object_or_404(GlossVideo, gloss_sn=videoid, version=0)
-    
+
+    if django_mobile.get_flavour(request) == 'mobile':
+        videourl = video.get_mobile_url()
+    else:
+        videourl = video.get_absolute_url()
+
     return render_to_response("iframe.html",
-                              {'videourl': video.get_absolute_url(),
+                              {'videourl': videourl,
                                'posterurl': video.poster_url(),
                                })
-    
-    
-    
+
+
+
