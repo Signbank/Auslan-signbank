0abe9a5d75ceac34c55bbf637b3b831e82141fbd
Steve Cassidy <Steve.Cassidy@mq.edu.au>
2013-05-26 22:39:14 +1000

    Fix problems with queries containing special characters and with pagination

--- signbank/dictionary/update.py
+++ signbank/dictionary/update.py
@@ -3,64 +3,92 @@ from django.template import Context, RequestContext, loader
 from django.http import Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.urlresolvers import reverse
-from django.conf import settings 
+from django.conf import settings
 from django.contrib.auth.decorators import permission_required
 from signbank.log import debug
-
+from tagging.models import TaggedItem, Tag
 import os, shutil
 
 from signbank.dictionary.models import *
-from signbank.dictionary.forms import * 
- 
+from signbank.dictionary.forms import *
+
 def update_gloss(request, glossid, version='dictionary'):
     """View to update a gloss from the form displayed on the staff view"""
-    
+
     if not request.user.has_perm('dictionary.change_gloss'):
-        return HttpResponseForbidden("Gloss Update Not Allowed")    
-    
+        return HttpResponseForbidden("Gloss Update Not Allowed")
+
     thisgloss = None
-    confirm_form = None 
+    confirm_form = None
     if request.method == "POST":
-        
+
         thisgloss = get_object_or_404(Gloss, id=glossid)
-        
+
         update_form = GlossModelForm(request.POST, instance=thisgloss)
-        
+
         if update_form.is_valid():
-            
+
             update_form.save()
-            
-        
+
+
         referer = request.META['HTTP_REFERER']
-        
-        return render_to_response("dictionary/update_result.html", 
+
+        return render_to_response("dictionary/update_result.html",
                               {'update_form': update_form,
                                'confirm_form': confirm_form,
                                'gloss' : thisgloss,
-                               'referer': referer, 
+                               'referer': referer,
                                },
                               context_instance=RequestContext(request))
 
+def add_tag(request, glossid, version='dictionary'):
+    """View to add a tag to a gloss"""
+
+    print "add_tag"
+    if request.method == "POST":
+        thisgloss = get_object_or_404(Gloss, id=glossid)
+
+        form = TagUpdateForm(request.POST)
+        if form.is_valid():
+            print form.cleaned_data
+
+            tag = form.cleaned_data['tag']
+
+            if form.cleaned_data['delete']:
+                # get the relevant TaggedItem
+                ti = get_object_or_404(TaggedItem, object_id=thisgloss.id, tag=tag)
+                ti.delete()
+            else:
+                Tag.objects.add_tag(thisgloss, tag)
+        else:
+            print "form not valid"
+
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+
+
 def update_video(request, glossid, version='dictionary'):
     """View to update the video for a gloss from the form displayed on the staff view"""
-    
+
     if not request.user.has_perm('dictionary.update_video'):
         return HttpResponseForbidden("Video Upload Not Allowed")
-        
-        
+
+
     keyword = None
-    n = None 
+    n = None
     videofile = None
     status = "form"
-    
+
     thisgloss = get_object_or_404(Gloss, id=glossid)
 
     if request.method == "POST":
         form = VideoUpdateForm(request.POST, request.FILES)
-        if form.is_valid(): 
+        if form.is_valid():
             if form.cleaned_data.has_key('videofile'):
                 video = form.cleaned_data['videofile']
-                # copy to comment video location 
+                # copy to comment video location
                 basename = os.path.split(video.name)[-1]
                 # this will be the path to the video, relative to MEDIA_ROOT
                 videofile = os.path.join(settings.VIDEO_UPLOAD_LOCATION, basename)
@@ -68,22 +96,22 @@ def update_video(request, glossid, version='dictionary'):
                 targetdir = os.path.join(settings.MEDIA_ROOT, settings.VIDEO_UPLOAD_LOCATION)
                 if not os.path.exists(targetdir):
                     os.mkdir(targetdir)
-                    
+
                 videoloc = os.path.join(settings.MEDIA_ROOT, videofile)
                 video.rename(videoloc)
-                
+
         elif request.POST.has_key("confirmvideofile"):
             # we're in the second stage confirmation
             videofile = request.POST['confirmvideofile']
             fullpath = os.path.join(settings.MEDIA_ROOT, videofile)
-            
+
             if request.POST.has_key("Cancel"):
                 if request.POST['Cancel'] == "cancel":
                     # remove the pending video file
                     if os.access(fullpath, os.F_OK):
                         os.unlink(fullpath)
                     status = "cancelled"
-            else: 
+            else:
                 # copy video to proper location
                 newlocation = os.path.join(settings.MEDIA_ROOT, thisgloss.get_video_save_location())
                 # backup existing file if any
@@ -92,31 +120,31 @@ def update_video(request, glossid, version='dictionary'):
                     if os.access(backup, os.F_OK):
                         os.unlink(backup)
                     shutil.copy(newlocation, backup)
-                
-                
+
+
                 # need to make sure the target directory is there
                 try:
                     os.makedirs(os.path.dirname(newlocation))
                 except:
                     pass
-                
+
                 # need shutil.copy here since we might be on different devices
                 shutil.copy(fullpath, newlocation)
                 os.unlink(fullpath)
                 #os.rename(fullpath, newlocation)
                 debug("Replaced video file: %s" % newlocation)
                 status = "completed"
-    else: 
+    else:
         form = VideoUpdateForm()
 
-    return render_to_response("dictionary/update_video.html", 
-                              {'form': form, 
+    return render_to_response("dictionary/update_video.html",
+                              {'form': form,
                                'gloss' : thisgloss,
-                               'keyword': keyword, 
+                               'keyword': keyword,
                                'n': n,
-                               'videofile': videofile, 
+                               'videofile': videofile,
                                'status': status,
                                },
                               context_instance=RequestContext(request))
- 
-    
+
+
--- signbank/dictionary/urls.py
+++ signbank/dictionary/urls.py
@@ -4,26 +4,27 @@ from signbank.dictionary.forms import *
 
 
 urlpatterns = patterns('',
-    
+
     # index page is just the search page
     (r'^$', 'signbank.dictionary.views.index'),
 
     # we use the same view for a definition and for the feedback form on that
     # definition, the first component of the path is word or feedback in each case
-    (r'^(?P<viewname>(words|feedback))/(?P<keyword>.+)-(?P<n>\d+).html$', 
+    (r'^(?P<viewname>(words|feedback))/(?P<keyword>.+)-(?P<n>\d+).html$',
             'signbank.dictionary.views.word'),
-    
+
     url(r'^tag/(?P<tag>[^/]*)/?$', 'signbank.dictionary.views.taglist'),
-                
+
     # and and alternate view for direct display of a gloss
     (r'gloss/(?P<idgloss>.+).html$', 'signbank.dictionary.views.gloss'),
-   
+
     url(r'^search/$', 'signbank.dictionary.views.search', name="dictionary-search"),
     (r'^update/gloss/(?P<glossid>\d+)$', 'signbank.dictionary.update.update_gloss'),
+    (r'^update/tag/(?P<glossid>\d+)$', 'signbank.dictionary.update.add_tag'),
     (r'^update/video/(?P<glossid>\d+)$', 'signbank.dictionary.update.update_video'),
-    
+
     (r'^ajax/keyword/(?P<prefix>.*)$', 'signbank.dictionary.views.keyword_value_list'),
-    
+
     (r'^missingvideo.html$', 'signbank.dictionary.views.missing_video_view'),
 
     (r'^export.csv', 'signbank.dictionary.views.csv_export'),
