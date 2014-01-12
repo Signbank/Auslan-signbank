bd825baebe2b11cb87e8747ea55b443b29ad6692
Steve Cassidy <Steve.Cassidy@mq.edu.au>
2014-01-12 16:51:30 +1100

    fix tag view

--- signbank/dictionary/tagviews.py
+++ signbank/dictionary/tagviews.py
@@ -1,6 +1,10 @@
 
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from tagging.models import Tag, TaggedItem
+from signbank.dictionary.models import Gloss
 
 def taglist(request, tag=None, version='dictionary'):
     """View of a list of tags or a list of signs with a given tag"""
