8ce892f9d6a30a1e8b5b0bd737ccb7057b745b00
stevecassidy <stevecassidy@localhost>
2014-04-11 10:04:19 +1000

    Cut down admin site linked to menus for staff users.

--- signbank/adminsite.py
+++ signbank/adminsite.py
@@ -1,6 +1,6 @@
 from django.contrib.admin.sites import AdminSite
-from django.contrib.auth.models import User, Group
-from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 from pages.models import Page
 from pages.admin import PageAdmin
@@ -8,4 +8,3 @@ from pages.admin import PageAdmin
 publisher_admin = AdminSite('pageadmin')
 publisher_admin.register(Page, PageAdmin)
 publisher_admin.register(User, UserAdmin)
-publisher_admin.register(Group, GroupAdmin)
