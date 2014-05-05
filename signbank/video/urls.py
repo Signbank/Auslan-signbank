37b443abf0609d395e4cc6e58efc5c667a999d65
Steve Cassidy <Steve.Cassidy@mq.edu.au>
2014-05-05 13:03:13 +1000

    Use more explicit pattern for video id in URL to prevent matching trailing slash.

--- signbank/video/urls.py
+++ signbank/video/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import *
 
 urlpatterns = patterns('',
     
-    (r'^video/(?P<videoid>.*)$', 'signbank.video.views.video'),
+    (r'^video/(?P<videoid>\d+)$', 'signbank.video.views.video'),
     (r'^upload/', 'signbank.video.views.addvideo'),
-    (r'^delete/(?P<videoid>.*)$', 'signbank.video.views.deletevideo'),
-    (r'^poster/(?P<videoid>.*)$', 'signbank.video.views.poster'),
-    (r'^iframe/(?P<videoid>.*)$', 'signbank.video.views.iframe'),
+    (r'^delete/(?P<videoid>\d+)$', 'signbank.video.views.deletevideo'),
+    (r'^poster/(?P<videoid>\d+)$', 'signbank.video.views.poster'),
+    (r'^iframe/(?P<videoid>\d+)$', 'signbank.video.views.iframe'),
 )
 
 
