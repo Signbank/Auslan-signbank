bc84221d8c30b069875ef8f6e2b06a2ee78d9a03
Steve Cassidy <steve.cassidy@mq.edu.au>
2014-05-10 20:27:15 +1000

    add script to create default groups and sample users, fix permission for viewing interpreter notes page

---
+++ signbank/dictionary/management/commands/permissions.py
@@ -0,0 +1,112 @@
+"""Set up default groups and permissions"""
+
+from django.core.management.base import BaseCommand, CommandError  
+from signbank.dictionary.models import Gloss
+from django.contrib.auth.models import User, Group, Permission
+
+
+advanced_search = Permission.objects.get(codename='search_gloss')
+view_adv_properties = Permission.objects.get(codename='view_advanced_properties')
+export_csv = Permission.objects.get(codename='export_csv')
+add_video = Permission.objects.get(codename='update_video')
+create_signs = Permission.objects.get(codename='add_gloss')
+publish = Permission.objects.get(codename='can_publish')
+delete_unpublished = Permission.objects.get(codename='can_delete_unpublished')
+delete_published = Permission.objects.get(codename='can_delete_published')
+view_interp_note = Permission.objects.get(codename='view_interpreterfeedback')
+create_interp_note = Permission.objects.get(codename='add_interpreterfeedback')
+delete_interp_note = Permission.objects.get(codename='delete_interpreterfeedback')
+
+
+class Command(BaseCommand):
+     
+    help = 'set up default groups and permissions'
+    args = ''
+
+    def handle(self, *args, **options):
+
+        
+            # Publisher
+            publisher, created = Group.objects.get_or_create(name='Publisher')
+            publisher.permissions.add(advanced_search)
+            publisher.permissions.add(view_adv_properties)
+            publisher.permissions.add(export_csv)
+            publisher.permissions.add(add_video)
+            publisher.permissions.add(create_signs)
+            publisher.permissions.add(publish)
+            publisher.permissions.add(delete_unpublished)
+            publisher.permissions.add(delete_published)
+            publisher.permissions.add(view_interp_note)
+            publisher.permissions.add(create_interp_note)
+            publisher.permissions.add(delete_interp_note)
+            
+            # Editor
+            editor, created = Group.objects.get_or_create(name='Editor')
+            editor.permissions.add(advanced_search)
+            editor.permissions.add(view_adv_properties)
+            editor.permissions.add(export_csv)
+            editor.permissions.add(add_video)
+            editor.permissions.add(create_signs)
+            editor.permissions.add(delete_unpublished)
+            editor.permissions.add(view_interp_note)
+            editor.permissions.add(create_interp_note)
+            editor.permissions.add(delete_interp_note)
+            
+            # Interpreter
+            interpreter, created = Group.objects.get_or_create(name='Interpreter')
+            interpreter.permissions.add(advanced_search)
+            interpreter.permissions.add(view_adv_properties)
+            interpreter.permissions.add(export_csv)
+            interpreter.permissions.add(view_interp_note)
+            interpreter.permissions.add(create_interp_note)
+            
+            
+            # Interpreter Supervisor
+            supervisor, created = Group.objects.get_or_create(name='Interpreter Supervisor')
+            supervisor.permissions.add(advanced_search)
+            supervisor.permissions.add(view_adv_properties)
+            supervisor.permissions.add(export_csv)
+            supervisor.permissions.add(view_interp_note)
+            supervisor.permissions.add(create_interp_note)
+            supervisor.permissions.add(delete_interp_note)
+            
+            
+            # Researcher
+            researcher, created = Group.objects.get_or_create(name='Researcher')
+            researcher.permissions.add(advanced_search)
+            researcher.permissions.add(view_adv_properties)
+            researcher.permissions.add(export_csv)
+            
+            # Observer
+            observer, created = Group.objects.get_or_create(name='Observer')
+            observer.permissions.add(advanced_search)
+            observer.permissions.add(view_adv_properties)
+            observer.permissions.add(export_csv)
+            
+            
+            # Public
+            public, created = Group.objects.get_or_create(name='Public')
+            
+            
+            
+            # create sample users if we don't have them already
+            if len(User.objects.filter(username='publisher')) == 0:
+            
+                # create some sample users            
+                peter = User.objects.create_user('publisher', 'example@example.com', 'publisher', first_name='Peter', last_name='Publisher')
+                eric = User.objects.create_user('editor', 'example@example.com', 'editor', first_name='Eric', last_name='Editor')
+                ian = User.objects.create_user('interpreter', 'example@example.com', 'interpreter', first_name='Ian', last_name='Interpreter')
+                boss = User.objects.create_user('boss', 'example@example.com', 'boss', first_name='Interpreter', last_name='Boss')
+                roger = User.objects.create_user('researcher', 'example@example.com', 'researcher', first_name='Roger', last_name='Researcher')
+                olive = User.objects.create_user('observer', 'example@example.com', 'observer', first_name='Olive', last_name='Observer')
+                public = User.objects.create_user('public', 'example@example.com', 'public', first_name='Pamela', last_name='Public')
+            
+        
+                peter.groups.add(publisher)
+                eric.groups.add(editor)
+                ian.groups.add(interpreter)
+                boss.groups.add(supervisor)
+                roger.groups.add(researcher)
+                olive.groups.add(observer)
+            
+            
