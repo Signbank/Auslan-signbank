0372fee31523cec49e1f7aaf44c1c157878f4657
Steve Cassidy <steve.cassidy@mq.edu.au>
2014-06-24 08:56:35 +1000

    remove old dir and fix admin view glitch

--- signbank/settings/development.py
+++ signbank/settings/development.py
@@ -8,8 +8,8 @@ TEMPLATE_DIRS = (
 # show emails on the console
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-#PRIMARY_CSS = "bootstrap_css/bsl.css"
-PRIMARY_CSS = "bootstrap_css/auslan.css"
+PRIMARY_CSS = "bootstrap_css/bsl.css"
+#PRIMARY_CSS = "bootstrap_css/auslan.css"
 
 
 # defines the aspect ratio for videos
@@ -17,11 +17,11 @@ PRIMARY_CSS = "bootstrap_css/auslan.css"
 
 
 # what do we call this signbank?
-#LANGUAGE_NAME = "BSL"
-#COUNTRY_NAME = "the UK"
+LANGUAGE_NAME = "BSL"
+COUNTRY_NAME = "the UK"
 
 # show/don't show sign navigation
-#SIGN_NAVIGATION = False
+SIGN_NAVIGATION = False
 
 # show the number signs page or an under construction page?
 #SHOW_NUMBERSIGNS = False
@@ -30,10 +30,10 @@ PRIMARY_CSS = "bootstrap_css/auslan.css"
 #ADVANCED_SEARCH = False
 
 # which definition fields do we show and in what order?
-#DEFINITION_FIELDS = []
+DEFINITION_FIELDS = []
 
 
-#ADMIN_RESULT_FIELDS = ['idgloss', 'annotation_idgloss']
+ADMIN_RESULT_FIELDS = ['idgloss', 'annotation_idgloss']
 
 
 GLOSS_VIDEO_DIRECTORY = 'glossvideo'
