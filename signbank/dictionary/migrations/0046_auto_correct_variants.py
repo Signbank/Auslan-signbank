236d092eb6aa302617b498394c7e527f74b4d1cd
Ehsan Keshavarzian <ehsan-keshavarzian@users.noreply.github.com>
2015-11-15 15:59:05 +1100

    auto correct variant relations based on naming convention

---
+++ signbank/dictionary/migrations/0046_auto_correct_variants.py
@@ -0,0 +1,102 @@
+from south.v2 import DataMigration
+import re
+from django.db import transaction
+import os
+
+
+class Migration(DataMigration):
+    
+    def forwards(self, orm):
+        transaction.set_autocommit(True)
+        for gloss in orm.Gloss.objects.all():
+            pattern = re.compile(r"\w*.*\w+[\d]+(?=[a-z]$)", re.IGNORECASE)
+            result = pattern.search(gloss.idgloss)
+            if result:
+                gloss_keyword = result.group()
+                if gloss.idgloss == gloss_keyword + "a":
+                    orm.Relation.objects.filter(source=gloss,
+                                            role="variant").delete()
+                else:
+                    orm.Relation.objects.filter(source=gloss,
+                                            role="variant").delete()
+                    root = list(orm.Gloss.objects.filter(idgloss = gloss_keyword + "a"))
+                    if root:
+                        root = root[0]
+                        relation, created = orm.Relation.objects.get_or_create(source=gloss,
+                                                           target=root,
+                                                           role="variant")
+                        if created:
+                            relation.save()
+                    else:
+                        dict_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+                        report_file = os.path.join(dict_directory, 'migration report.txt')
+                        with open(report_file, 'a') as report:
+                            report.write('Gloss %s has no root \n' % gloss.idgloss)
+    
+    def backwards(self, orm):
+        raise RuntimeError("Cannot reverse this migration.")
+    
+    models = {
+        u'dictionary.dialect': {
+            'Meta': {'ordering': "['language', 'name']", 'object_name': 'Dialect'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['dictionary.Language']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        u'dictionary.gloss': {
+            'Meta': {'ordering': "['idgloss']", 'object_name': 'Gloss'},
+            'StemSN': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'annotation_idgloss': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'aslgloss': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'asloantf': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'asltf': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'blend': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'blendtf': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'bslgloss': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'bslloantf': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'bsltf': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'compound': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'comptf': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'dialect': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['dictionary.Dialect']", 'symmetrical': 'False'}),
+            'domhndsh': ('django.db.models.fields.CharField', [], {'max_length': '5', 'null': 'True', 'blank': 'True'}),
+            'final_domhndsh': ('django.db.models.fields.CharField', [], {'max_length': '5', 'null': 'True', 'blank': 'True'}),
+            'final_loc': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'final_palm_orientation': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'final_relative_orientation': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'final_secondary_loc': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'final_subhndsh': ('django.db.models.fields.CharField', [], {'max_length': '5', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'idgloss': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'inWeb': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'blank': 'True'}),
+            'initial_palm_orientation': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'initial_relative_orientation': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'initial_secondary_loc': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'inittext': ('django.db.models.fields.CharField', [], {'max_length': "'50'", 'blank': 'True'}),
+            'isNew': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'blank': 'True'}),
+            'language': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['dictionary.Language']", 'symmetrical': 'False'}),
+            'locprim': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'locsecond': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'morph': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'sedefinetf': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'segloss': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'sense': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'sn': ('django.db.models.fields.IntegerField', [], {'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'subhndsh': ('django.db.models.fields.CharField', [], {'max_length': '5', 'null': 'True', 'blank': 'True'})
+        },
+        u'dictionary.language': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Language'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        u'dictionary.relation': {
+            'Meta': {'ordering': "['source']", 'object_name': 'Relation'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'role': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'relation_sources'", 'to': u"orm['dictionary.Gloss']"}),
+            'target': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'relation_targets'", 'to': u"orm['dictionary.Gloss']"})
+        }
+    }
+    
+    complete_apps = ['dictionary']
